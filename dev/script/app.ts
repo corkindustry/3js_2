9c26a62fe328347549704aebba4e8eb32c5c85a6
corkindustry <falsefriend13@gmail.com>
2018-12-31 14:39:47 -0500

    more typescripting

--- dev/script/app.ts
+++ dev/script/app.ts
@@ -1,35 +1,34 @@
 import * as THREE from 'three';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-var renderer = new THREE.WebGLRenderer( { alpha: true });
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff, 0);
 document.body.appendChild(renderer.domElement);
 
-var light = new THREE.AmbientLight(0xffffff, 0.8);
+const light = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(light);
 
-var light1 = new THREE.PointLight(0xffffff, 0.5);
+const light1 = new THREE.PointLight(0xffffff, 0.5);
 scene.add(light1);
 
-var geometry = new THREE.IcosahedronGeometry(20, 0);
-var material = new THREE.MeshStandardMaterial(
+const geometry = new THREE.IcosahedronGeometry(20, 0);
+const material = new THREE.MeshStandardMaterial(
   {
     color: 0x45b1c2,
     roughness: 0.7,
   }
 );
 
-var cube: any = new THREE.Mesh(geometry, material);
+const cube: any = [];
 
-cube = [];
-for (var i = 0; i < 500; i++) {
+for (let i = 0; i < 500; i++) {
   cube[i] = new THREE.Mesh(geometry, material);
-  var x = Math.floor((Math.random() * window.innerWidth) - (window.innerWidth / 2));
-  var y = Math.floor((Math.random() * window.innerHeight) - (window.innerHeight / 2));
-  var z = -(Math.floor(Math.random() * 6001) + 1);
+  let x = Math.floor((Math.random() * window.innerWidth) - (window.innerWidth / 2));
+  let y = Math.floor((Math.random() * window.innerHeight) - (window.innerHeight / 2));
+  let z = -(Math.floor(Math.random() * 6001) + 1);
   cube[i].position.set(x, y, z);
   scene.add(cube[i]);
 }
@@ -38,17 +37,14 @@ camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
-  for (var i = 0; i < 500; i++) {
-    var x = Math.floor((Math.random() * 100) + 1) / 10000;
-    // console.log(x);
-    var y = Math.floor((Math.random() * 100) + 1) / 12000;
-    // console.log(y);
+  for (let i = 0; i < 500; i++) {
+    let x = Math.floor((Math.random() * 100) + 1) / 10000;
+    let y = Math.floor((Math.random() * 100) + 1) / 12000;
     cube[i].rotation.x += x;
     cube[i].rotation.y += y;
     cube[i].position.z -= 1;
     if (cube[i].position.z < -3000) {
       cube[i].position.z = (Math.floor(Math.random() * 2001) + 1);
-      // console.log("reset " + cube[i]);
     }
   }
   renderer.render(scene, camera);
